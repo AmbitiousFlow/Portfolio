6529df495917faa4ead205d51b665ec23b7d4b9b
AmbitiousFlow <mohamed.lafrouh@gmail.com>
2025-06-26 11:50:35 +0100

    Update About section with new name and bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,17 +9,21 @@ export default function About() {
     <section className="my-28 mb-20" id="about">
       <section className="flex flex-col gap-6">
         <h1 className="xl:text-8xl lg:text-7xl md:text-6xl text-4xl text-center font-monsterate">
-          Hi I&apos;am <span className="text-primary">Kyle Myre</span>
+          Hi I&apos;am <span className="text-primary">MOHAMED LAFROUH</span>
         </h1>
         <p className="lg:text-center text-justify lg:text-xl text-xl sm: font-inconsolata">
-          Good day. I am Mohamed Lafrouh, also known as Kyle Myre. I am 20 years
-          of age and I am a full stack web developer/software developer. I have
-          studied web development for a number of years and have made a few
-          contacts in my development career.
+          Good day, My name is Mohamed Lafrouh, also known as Ambitious Flow. I
+          am 20 years old and work as a full-stack web developer and software
+          developer. I have spent several years studying web development and
+          have built valuable connections throughout my journey in the tech
+          industry.
         </p>
         <div className="flex lg:flex-row flex-col gap-2 justify-center font-inconsolata text-2xl font-semibold">
-          <a className="bg-primary text-white p-3 flex flex-row items-center gap-3 hover:bg-opacity-80 duration-500 transition-all">
-            <Linkedin  className="size-7" /> <span>Linkedin</span>
+          <a
+            href="https://www.linkedin.com/in/mohamed-lafrouh-7972a82a2/"
+            className="bg-primary text-white p-3 flex flex-row items-center gap-3 hover:bg-opacity-80 duration-500 transition-all"
+          >
+            <Linkedin className="size-7" /> <span>Linkedin</span>
           </a>
           <a
             className="bg-primary text-white p-3 flex flex-row items-center gap-3 hover:bg-opacity-80 duration-500 transition-all"
@@ -34,7 +38,6 @@ export default function About() {
           >
             <Mail className="size-7" /> <span>Mail</span>
           </a>
-
         </div>
       </section>
     </section>
